d894930dd0a228de95d764716f7a8952ba2f7960
rushikeshghuge <rushikeshghuge993@gmail.com>
2023-12-20 11:12:46 +0530

    workflows: added

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/CryptoTracker/',
   build: {
     chunkSizeWarningLimit: 1000, // Set your desired limit in kilobytes
   },
-  base: '/CryptoTracker/',
   
 });
